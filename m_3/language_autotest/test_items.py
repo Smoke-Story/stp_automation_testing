37b30bd81e9a1dfe7870e7269980750b3692c252
Smoke-Story <48931527+Smoke-Story@users.noreply.github.com>
2025-06-13 05:18:36 +0300

    Files of 'language project' updated

--- m_3/language_autotest/test_items.py
+++ m_3/language_autotest/test_items.py
@@ -1,7 +1,8 @@
 import pytest
+from time import sleep
 from selenium.webdriver.common.by import By
-
 
 def test_item_language(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    sleep(3) # что бы можно было убедиться в версии "es"
